78e1e38e5de7ac45a3849a3967ab0560308b6ce7
Marco LUCARELLA <marco.lucarella@amadeus.com>
2022-12-22 23:34:36 +0100

    Api testing

--- test/api.test.ts
+++ test/api.test.ts
@@ -39,37 +39,93 @@ describe('API', () => {
             expect(ObjectsCache.get).toBeCalledTimes(2)
             expect(ObjectsCache.get).toHaveBeenNthCalledWith(1, expect.stringMatching(new RegExp(kProject)))
             expect(ObjectsCache.get).toHaveBeenNthCalledWith(2, expect.stringMatching(new RegExp(kBoardId.toString())))
+            expect(ObjectsCache.add).toBeCalledTimes(2)
+            expect(ObjectsCache.add).toHaveBeenNthCalledWith(1, expect.stringMatching(new RegExp(kProject)), [kBoard])
+            expect(ObjectsCache.add).toHaveBeenNthCalledWith(2, expect.stringMatching(new RegExp(kBoardId.toString())), [kSprint])
             expect(JiraClient.getBoards).toBeCalledTimes(1)
             expect(JiraClient.getBoards).toHaveBeenNthCalledWith(1, kProject, 1)
             expect(JiraClient.getSprints).toBeCalledTimes(1)
             expect(JiraClient.getSprints).toHaveBeenNthCalledWith(1, kBoardId, ['active'], 1)
         })
-        // test('cached board, requested sprint', async () => {
-        //     (ObjectsCache.get as jest.MockedFunction<any>)
-        //         .mockReturnValueOnce({ data: [kBoard] })
-        //         .mockReturnValueOnce(null);
-        //     // (JiraClient.getBoards as jest.MockedFunction<any>).mockReturnValueOnce([]);
-        //     (JiraClient.getSprints as jest.MockedFunction<any>).mockReturnValueOnce([kSprint]);
+        test('cached board, requested sprint', async () => {
+            (ObjectsCache.get as jest.MockedFunction<any>)
+                .mockReturnValueOnce({ data: [kBoard] })
+                .mockReturnValueOnce(null);
+            (JiraClient.getSprints as jest.MockedFunction<any>).mockReturnValueOnce([kSprint]);
 
-        //     expect(await API.jira.getActiveSprint(kProject)).toEqual(kSprint)
-        // })
-        // test('board not found', async () => {
-        //     (ObjectsCache.get as jest.MockedFunction<any>).mockReturnValueOnce(null);
-        //     (JiraClient.getBoards as jest.MockedFunction<any>).mockReturnValueOnce([]);
-        //     // (JiraClient.getSprints as jest.MockedFunction<any>).mockReturnValueOnce([]);
+            expect(await API.jira.getActiveSprint(kProject)).toEqual(kSprint)
 
-        //     expect(await API.jira.getActiveSprint(kProject)).toEqual(null)
-        // })
-        // test.todo('board found, sprint not found')
+            expect(ObjectsCache.get).toBeCalledTimes(2)
+            expect(ObjectsCache.get).toHaveBeenNthCalledWith(1, expect.stringMatching(new RegExp(kProject)))
+            expect(ObjectsCache.get).toHaveBeenNthCalledWith(2, expect.stringMatching(new RegExp(kBoardId.toString())))
+            expect(ObjectsCache.add).toBeCalledTimes(1)
+            expect(ObjectsCache.add).toHaveBeenNthCalledWith(1, expect.stringMatching(new RegExp(kBoardId.toString())), [kSprint])
+            expect(JiraClient.getBoards).not.toBeCalled()
+            expect(JiraClient.getSprints).toBeCalledTimes(1)
+            expect(JiraClient.getSprints).toHaveBeenNthCalledWith(1, kBoardId, ['active'], 1)
+        })
+        test('cached board, cached sprint', async () => {
+            (ObjectsCache.get as jest.MockedFunction<any>)
+                .mockReturnValueOnce({ data: [kBoard] })
+                .mockReturnValueOnce({ data: [kSprint] });
+
+            expect(await API.jira.getActiveSprint(kProject)).toEqual(kSprint)
+
+            expect(ObjectsCache.get).toBeCalledTimes(2)
+            expect(ObjectsCache.get).toHaveBeenNthCalledWith(1, expect.stringMatching(new RegExp(kProject)))
+            expect(ObjectsCache.get).toHaveBeenNthCalledWith(2, expect.stringMatching(new RegExp(kBoardId.toString())))
+            expect(ObjectsCache.add).not.toBeCalled()
+            expect(JiraClient.getBoards).not.toBeCalled()
+            expect(JiraClient.getSprints).not.toBeCalled()
+        })
+        test('board not found', async () => {
+            (ObjectsCache.get as jest.MockedFunction<any>).mockReturnValueOnce(null);
+            (JiraClient.getBoards as jest.MockedFunction<any>).mockReturnValueOnce([]);
+
+            expect(await API.jira.getActiveSprint(kProject)).toEqual(null)
+
+            expect(ObjectsCache.get).toBeCalledTimes(1)
+            expect(ObjectsCache.get).toHaveBeenNthCalledWith(1, expect.stringMatching(new RegExp(kProject)))
+            expect(ObjectsCache.add).toBeCalledTimes(1)
+            expect(ObjectsCache.add).toHaveBeenNthCalledWith(1, expect.stringMatching(new RegExp(kProject)), [])
+            expect(JiraClient.getBoards).toBeCalledTimes(1)
+            expect(JiraClient.getBoards).toHaveBeenNthCalledWith(1, kProject, 1)
+            expect(JiraClient.getSprints).not.toBeCalled()
+        })
+        test('board found, sprint not found', async () => {
+            (ObjectsCache.get as jest.MockedFunction<any>).mockReturnValueOnce(null).mockReturnValueOnce(null);
+            (JiraClient.getBoards as jest.MockedFunction<any>).mockReturnValueOnce([kBoard]);
+            (JiraClient.getSprints as jest.MockedFunction<any>).mockReturnValueOnce([]);
+
+            expect(await API.jira.getActiveSprint(kProject)).toEqual(null)
+
+            expect(ObjectsCache.get).toBeCalledTimes(2)
+            expect(ObjectsCache.get).toHaveBeenNthCalledWith(1, expect.stringMatching(new RegExp(kProject)))
+            expect(ObjectsCache.get).toHaveBeenNthCalledWith(2, expect.stringMatching(new RegExp(kBoardId.toString())))
+            expect(ObjectsCache.add).toBeCalledTimes(2)
+            expect(ObjectsCache.add).toHaveBeenNthCalledWith(1, expect.stringMatching(new RegExp(kProject)), [kBoard])
+            expect(ObjectsCache.add).toHaveBeenNthCalledWith(2, expect.stringMatching(new RegExp(kBoardId.toString())), [])
+            expect(JiraClient.getBoards).toBeCalledTimes(1)
+            expect(JiraClient.getBoards).toHaveBeenNthCalledWith(1, kProject, 1)
+            expect(JiraClient.getSprints).toBeCalledTimes(1)
+            expect(JiraClient.getSprints).toHaveBeenNthCalledWith(1, kBoardId, ['active'], 1)
+        })
     })
 
     describe('getActiveSprintName', () => {
-        // test('not found', async () => {
-        //     (ObjectsCache.get as jest.MockedFunction<any>).mockReturnValueOnce(null);
-        //     (JiraClient.getBoards as jest.MockedFunction<any>).mockReturnValueOnce([]);
+        test('found', async () => {
+            (ObjectsCache.get as jest.MockedFunction<any>)
+                .mockReturnValueOnce({ data: [kBoard] })
+                .mockReturnValueOnce({ data: [kSprint] });
 
-        //     expect(await API.jira.getActiveSprintName(kProject)).toEqual('')
-        // })
+            expect(await API.jira.getActiveSprintName(kProject)).toEqual(kSprintName)
+        })
+        test('not found', async () => {
+            (ObjectsCache.get as jest.MockedFunction<any>).mockReturnValueOnce(null);
+            (JiraClient.getBoards as jest.MockedFunction<any>).mockReturnValueOnce([]);
+
+            expect(await API.jira.getActiveSprintName(kProject)).toEqual('')
+        })
     })
 
     afterEach(() => {
